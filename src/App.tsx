962e1910f8537dad4f0117a50975ceca1e02ff22
wagihmohamed <wagihm969@gmail.com>
2023-04-02 23:14:16 +0200

    init commit google maps

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { HomeScreen } from './screens/HomeScreen';
+
+function App() {
+  return <HomeScreen />;
+}
+
+export default App;
---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,32 @@
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
+
+export const HomeScreen = () => {
+  const center = useMemo(() => ({ lat: 30.0444, lng: 31.2357 }), []);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyD8cW7J-u1NvWYR5Os155W7nG6OO6Z_Wd8',
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <GoogleMap
+      zoom={10}
+      center={center}
+      mapContainerStyle={{
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <MarkerF
+        position={center}
+        icon={{
+          url: 'https://mui.com/static/images/avatar/1.jpg',
+          scaledSize: { width: 32, height: 32, equals: () => true },
+        }}
+      />
+    </GoogleMap>
+  );
+};
